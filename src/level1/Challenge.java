23a76c179602b8c85b7719b958b981eccab48be7
Keith <keith.groves@jointheleague.org>
2017-04-09 19:11:50 -0700

    add more challenges

---
+++ src/level1/Challenge.java
@@ -0,0 +1,25 @@
+package level1;
+
+import processing.core.PApplet;
+
+public class Challenge extends Head {
+	Roomba roomba;
+
+	public static void main(String[] args) {
+		PApplet.main("level1.Processing");
+	}
+
+	public Challenge(Roomba roomba) {
+		super(roomba);
+	}
+	/**
+	 * Without touching the wall reach the endZone
+	 */
+	public void initialize() {
+
+	}
+
+	public void loop() {
+	
+	}
+}
---
+++ src/level2/Challenge.java
@@ -0,0 +1,25 @@
+package level2;
+
+import level1.Sonar;
+import processing.core.PApplet;
+
+public class Challenge extends Head {
+	Roomba roomba;
+
+	public static void main(String[] args) {
+		PApplet.main("level2.Processing");
+	}
+
+	public Challenge(Roomba roomba) {
+		super(roomba);
+		Sonar sonar = new Sonar(roomba);
+	}
+
+	public void initialize() {
+
+	}
+
+	public void loop() {
+	
+	}
+}
